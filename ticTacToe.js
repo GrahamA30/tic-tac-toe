b64cd98a1bba73e6abe48efa393c034257ce225d
GrahamA30 <aj.graham17@gmail.com>
2020-05-07 13:24:17 -0400

    added isThreeInRow() method to gameBoard object

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,18 +1,60 @@
 const gameBoard = (() => {
     let board = [
-                    ["","",""],
-                    ["","",""],
-                    ["","",""]
+                    ["X","O","X"],
+                    ["O","","O"],
+                    ["O","","X"]
                 ];
     const addMark = (player, row, col) => {
         if(isEmpty(row,col)) board[row][col] = player.getMark();
     }
     const getBoard = () => board;
-    const isEmpty = (row, col) => board[row][col] == "" ? true : false; 
+    const isEmpty = (row, col) => board[row][col] == "" ? true : false;
+    const isThreeInRow = (player) =>{
+        let found =true;
+
+        for(let r=0;r < board.length;r++){
+            found = true;
+            for(let c=0;c < board[0].length;c++){
+                if(board[r][c] != player.getMark()) found = false; 
+            }
+            if(found == true) return true;
+        }
+        for(let c=0;c < board[0].length;c++){
+            found = true;
+            for(let r=0;r < board.length;r++){
+                if(board[r][c] != player.getMark()) found = false; 
+            }
+            if(found == true) return true;
+        }
+
+        let row =0;
+        let col = 0;
+        found =true;
+        while (row <board.length && col < board[0].length){
+            if(board[row][col] != player.getMark()) found = false;
+            row++;
+            col++;
+        }
+        if(found) return true;
+
+        row = 2;
+        col = 0;
+        found =true;
+        while (row >=0 && col < board[0].length){
+            if(board[row][col] != player.getMark()) found = false;
+            row--;
+            col++;
+        }
+        if(found) return true;
+
+        return false;
+
+    }  
 
     return {
       addMark,
-      getBoard
+      getBoard,
+      isThreeInRow
     };
   })();
 
