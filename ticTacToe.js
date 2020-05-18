86aba33aae0a6f8875fba4e33dd75cdfebc09cf1
GrahamA30 <aj.graham17@gmail.com>
2020-05-17 21:01:27 -0400

    added function to determine when the game is over

--- ticTacToe.js
+++ ticTacToe.js
@@ -9,6 +9,21 @@ const gameBoard = (() => {
     }
     const getBoard = () => board;
     const isEmpty = (row, col) => board[row][col] == "" ? true : false;
+    const clearBoard = () =>{
+      for(let r=0;r < board.length;r++){
+        for(let c=0;c < board[0].length;c++){
+            board[r][c] = ""; 
+        }
+      }
+    }
+    const isFilled = () =>{
+      for(let r=0;r < board.length;r++){
+        for(let c=0;c < board[0].length;c++){
+            if(board[r][c] != "") return false; 
+        }
+      }
+      return true;
+    }
     const isThreeInRow = (player) =>{
         let found =true;
 
@@ -55,28 +70,52 @@ const gameBoard = (() => {
       addMark,
       getBoard,
       isThreeInRow,
-      isEmpty
+      isEmpty,
+      isFilled,
+      clearBoard
     };
   })();
 
   const Player = (name, mark) => {
     let turn = false;
+    let score = 0;
+    const getScore = () => score;
     const getName = () => name;
     const getMark = () => mark;
     const getTurn = () => turn;
     const changeTurn = () => turn = !turn;
+    const addScore = () => score++;
 
-    return{getName, getMark, getTurn, changeTurn};
+
+    return{getName, getMark, getTurn, changeTurn, getScore, addScore};
   };
 
   const gameState = (() => {
     const player1 = Player("player1","X");
     const player2 = Player("player2","O");
     player1.changeTurn();
+    
+    let play = true;
 
     const displayWinner = (player) => console.log(player.getName());
+    const gameOver = () =>{
+      if(gameBoard.isThreeInRow(player1)){
+        console.log(player1.getName());
+        player1.addScore();
+        play = false;
+      }
+      else if(gameBoard.isThreeInRow(player2)){
+        console.log(player2.getName());
+        player2.addScore();
+        play = false;
+      }
+      else if (gameBoard.isFilled()){
+        play = false;
+      }
+
+    }
     const takeTurn = (row,col) =>{
-      if(gameBoard.isEmpty(row,col)){
+      if(gameBoard.isEmpty(row,col) && play){
         if(player1.getTurn()){
           gameBoard.addMark(player1,row,col);
         }
@@ -86,6 +125,7 @@ const gameBoard = (() => {
         player1.changeTurn();
         player2.changeTurn();
       }
+      gameOver();
       displayController.updateDisplay();
     }
 
